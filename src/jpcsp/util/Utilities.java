mirror/jpcsp

commit 2837664d53275651d77146aa8b514fb60a1b05a6

Improved performance of sceIoWrite when writing large amount of data.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@1573 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/util/Utilities.java
@@ -28,7 +28,9 @@ import jpcsp.MemoryMap;
 import jpcsp.Allegrex.CpuState;
 import jpcsp.HLE.Modules;
 import jpcsp.filesystems.*;
+import jpcsp.memory.IMemoryReader;
 import jpcsp.memory.IMemoryWriter;
+import jpcsp.memory.MemoryReader;
 import jpcsp.memory.MemoryWriter;
 
 import java.nio.Buffer;
@@ -345,11 +347,13 @@ public class Utilities {
     	Buffer buffer = Memory.getInstance().getBuffer(address, length);
     	if (buffer instanceof ByteBuffer) {
     		output.getChannel().write((ByteBuffer) buffer);
-    	} else {
-    		Memory mem = Memory.getInstance();
+    	} else if (length > 0) {
+    		byte[] bytes = new byte[length];
+    		IMemoryReader memoryReader = MemoryReader.getMemoryReader(address, 1, length);
     		for (int i = 0; i < length; i++) {
-    			output.writeByte(mem.read8(address + i));
+    			bytes[i] = (byte) memoryReader.readNext();
     		}
+    		output.write(bytes);
     	}
     }
 

commit 0ce0610238302373d7612da37ecae3581b430b53

Fixed typo introduced in r1573 (savedata was incorrectly saved).
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@1579 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/util/Utilities.java
@@ -349,7 +349,7 @@ public class Utilities {
     		output.getChannel().write((ByteBuffer) buffer);
     	} else if (length > 0) {
     		byte[] bytes = new byte[length];
-    		IMemoryReader memoryReader = MemoryReader.getMemoryReader(address, 1, length);
+    		IMemoryReader memoryReader = MemoryReader.getMemoryReader(address, length, 1);
     		for (int i = 0; i < length; i++) {
     			bytes[i] = (byte) memoryReader.readNext();
     		}