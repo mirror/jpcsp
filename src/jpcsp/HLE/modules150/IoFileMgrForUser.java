mirror/jpcsp

commit 28e9aec8ff07b537c625e3e1c009ebe3ba598b8f

Improved CryptoEngine with new findings: - Implemented PGD decryption; - Fixed SAVEDATA encryption/decryption alignment issues; - Added DRM mesg_led keys from firmware 6.00 and up; - Reversed and implemented amctrl.prx module's functions.
Added new ELF loading method for firmware 6.30 and up in Loader.

Implemented filter based rendering:
- Provided bilinear filter with custom resolutions (by Aredo);
- Added partial full screen support.

Improved screenshot taking in sceDisplay.

git-svn-id: http://jpcsp.googlecode.com/svn/trunk@2006 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/IoFileMgrForUser.java
@@ -63,6 +63,7 @@ import jpcsp.HLE.modules.HLEModuleManager;
 import jpcsp.HLE.modules.HLEStartModule;
 import jpcsp.HLE.modules.ThreadManForUser;
 import jpcsp.connector.PGDFileConnector;
+import jpcsp.crypto.CryptoEngine;
 import jpcsp.filesystems.SeekableDataInput;
 import jpcsp.filesystems.SeekableRandomFile;
 import jpcsp.filesystems.umdiso.UmdIsoFile;
@@ -1507,7 +1508,7 @@ public class IoFileMgrForUser implements HLEModule, HLEStartModule {
                     }
                     break;
                 }
-                //Get UMD file pointer.
+                // Get UMD file pointer.
                 case 0x01020004: {
                     if (Memory.isAddressGood(outdata_addr) && outlen >= 4) {
                         if (info.isUmdFile()) {
@@ -1629,22 +1630,64 @@ public class IoFileMgrForUser implements HLEModule, HLEStartModule {
                     }
                     break;
                 }
-                // Define decryption key (Kirk / AES-128?).
+                // Define decryption key (DRM by amctrl.prx).
                 case 0x04100001: {
                     if (Memory.isAddressGood(indata_addr) && inlen == 16) {
+                        CryptoEngine crypto = new CryptoEngine();
                         String keyHex = "";
-                        for (int i = 0; i < inlen; i++) {
-                            AES128Key[i] = (byte) mem.read8(indata_addr + i);
-                            keyHex += String.format("%02x", AES128Key[i] & 0xFF);
+
+                        if (pgdFileConnector == null) {
+                            pgdFileConnector = new PGDFileConnector();
+                        }
+
+                        try {
+                            SeekableRandomFile decFile = new SeekableRandomFile(PGDFileConnector.decryptedFileName, "rw");
+                            int fileLength = (int) info.readOnlyFile.length();
+                            byte[] inBuf = new byte[fileLength];
+                            byte[] outBuf = new byte[fileLength];
+                            byte[] headerBuf = new byte[0x30 + 0x10];
+                            byte[] keyBuf = new byte[0x10];
+                            byte[] hashBuf = new byte[0x10];
+
+                            info.readOnlyFile.readFully(inBuf);
+
+                            // Store the key.
+                            for (int i = 0; i < 0x10; i++) {
+                                keyBuf[i] = (byte) mem.read8(indata_addr + i);
+                                keyHex += String.format("%02x", keyBuf[i] & 0xFF);
+                            }
+
+                            // Decrypt 0x30 bytes at offset 0x30 to expose the first header.
+                            System.arraycopy(inBuf, 0x10, headerBuf, 0, 0x10);
+                            System.arraycopy(inBuf, 0x30, headerBuf, 0x10, 0x30);
+                            byte headerBufDec[] = crypto.DecryptPGD(headerBuf, 0x30 + 0x10, keyBuf);
+
+                            // Extract the decrypting parameters.
+                            System.arraycopy(headerBufDec, 0, hashBuf, 0, 0x10);
+                            int dataSize = (headerBufDec[0x14] & 0xFF) | ((headerBufDec[0x15] & 0xFF) << 8) |
+                                    ((headerBufDec[0x16] & 0xFF) << 16) | ((headerBufDec[0x17] & 0xFF) << 24);
+                            int chunkSize = (headerBufDec[0x18] & 0xFF) | ((headerBufDec[0x19] & 0xFF) << 8) |
+                                    ((headerBufDec[0x1A] & 0xFF) << 16) | ((headerBufDec[0x1B] & 0xFF) << 24);
+                            int hashOffset = (headerBufDec[0x1C] & 0xFF) | ((headerBufDec[0x1D] & 0xFF) << 8) |
+                                    ((headerBufDec[0x1E] & 0xFF) << 16) | ((headerBufDec[0x1F] & 0xFF) << 24);
+
+                            // Write the newly extracted hash at the top of the output buffer,
+                            // locate the data hash at hashOffset and start decrypting until
+                            // dataSize is reached.
+                            System.arraycopy(hashBuf, 0, outBuf, 0, 0x10);
+                            System.arraycopy(inBuf, hashOffset, outBuf, 0x10, dataSize);
+
+                            decFile.write(crypto.DecryptPGD(outBuf, dataSize + 0x10, keyBuf));
+                            decFile.close();
+                        } catch (Exception e) {
+                            // Ignore.
                         }
 
                         if (log.isDebugEnabled()) {
                             log.debug("hleIoIoctl get AES key " + keyHex);
                         }
 
-                        if (pgdFileConnector == null) {
-                            pgdFileConnector = new PGDFileConnector();
-                        }
+                        // Send the manually decrypted file generated just now.
                         info.readOnlyFile = pgdFileConnector.decryptPGDFile(info.filename, info.readOnlyFile, keyHex);
 
                         result = 0;

src/jpcsp/HLE/modules150/sceDisplay.java
@@ -85,6 +85,10 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
     private static final boolean useDebugGL = false;
     private static final int internalTextureFormat = GeCommands.TPSM_PIXEL_STORAGE_MODE_32BIT_ABGR8888;
 
+    // Screen default resolution
+    public int screenWidth = 480;
+    public int screenHeight = 272;
+
     // sceDisplayPixelFormats enum
     public static final int PSP_DISPLAY_PIXEL_FORMAT_565  = 0;
     public static final int PSP_DISPLAY_PIXEL_FORMAT_5551 = 1;
@@ -146,6 +150,7 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
     private int captureY;
     private int captureWidth;
     private int captureHeight;
+    private Robot captureRobot;
     public boolean getscreen = false;
 
     //Rotation vars
@@ -327,11 +332,17 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
     }
 
     public sceDisplay() throws LWJGLException {
-    	super(null, new PixelFormat().withBitsPerPixel(8).withAlphaBits(8).withStencilBits(8), null, new ContextAttribs().withDebug(useDebugGL));
-        setSize(480, 272);
+    	super(null, new PixelFormat().withBitsPerPixel(8).withAlphaBits(8).withStencilBits(8).withSamples(16), null, new ContextAttribs().withDebug(useDebugGL));
+        setSize(screenWidth, screenHeight);
+
         texFb = -1;
         startModules = false;
         tempSize = 0;
+    }
+
+    public void setScreenResolution(int width, int height) {
+        screenWidth = width;
+        screenHeight = height;
     }
 
     @Override
@@ -402,6 +413,17 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
     	if (saveGEToTexture) {
     		log.info("Saving GE to Textures");
     	}
+
+        captureX = Settings.getInstance().readInt("gui.windows.mainwindow.x") + 4;
+        captureY = Settings.getInstance().readInt("gui.windows.mainwindow.y") + 76;
+        captureWidth = getCanvasWidth();
+        captureHeight = getCanvasHeight();
+        try {
+            captureRobot = new Robot();
+            captureRobot.setAutoDelay(0);
+        } catch (Exception e) {
+            // Ignore.
+        }
     }
 
     @Override
@@ -676,12 +698,10 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
         VideoEngine.log.info("Only GE Graphics: " + onlyGEGraphics);
     }
 
-    //Screen rotation function
-    public void rotate(int angleid)
-    {
-        ang = angleid;
+    public void rotate(int angleId) {
+        ang = angleId;
 
-        switch(angleid){
+        switch(angleId){
             case 0: //Rotate screen - 90º CW
                 texS1 = texS2 = texS;
                 texT2 = texT3 = texT;
@@ -730,7 +750,7 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
         }
     }
 
-    public void savescreen() {
+    public void saveScreen() {
         int tag = 0;
         File screenshot = new File(State.discId + "-" + "Shot" + "-" + tag + ".png");
         File directory = new File(System.getProperty("user.dir"));
@@ -744,7 +764,7 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
 
         Rectangle rect = new Rectangle(captureX, captureY, captureWidth, captureHeight);
         try {
-            BufferedImage img = new Robot().createScreenCapture(rect);
+            BufferedImage img = captureRobot.createScreenCapture(rect);
             ImageIO.write(img, "png", screenshot);
             img.flush();
         } catch (Exception e) {
@@ -1280,7 +1300,7 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
         }
 
         if (getscreen) {
-        	savescreen();
+        	saveScreen();
         }
 	}
 

src/jpcsp/Loader.java
@@ -322,8 +322,13 @@ public class Loader {
             module.loadAddressHigh = baseAddress;
 
             // Load into mem
-            LoadELFProgram(f, module, baseAddress, elf, elfOffset);
-            LoadELFSections(f, module, baseAddress, elf, elfOffset);
+            if(Emulator.getInstance().getFirmwareVersion() >= 630) {
+                LoadELFProgram_630(f, module, baseAddress, elf, elfOffset);
+                LoadELFSections_630(f, module, baseAddress, elf, elfOffset);
+            } else {
+                LoadELFProgram(f, module, baseAddress, elf, elfOffset);
+                LoadELFSections(f, module, baseAddress, elf, elfOffset);
+            }
 
             // Relocate PRX
             if (elf.getHeader().requiresRelocation()) {
@@ -403,6 +408,12 @@ public class Loader {
 
     // ELF Loader
 
+     private void LoadELFProgram_630(ByteBuffer f, SceModule module, int baseAddress,
+        Elf32 elf, int elfOffset) throws IOException {
+         Emulator.log.info("6.30 firmware version's ELF program detected!");
+         LoadELFProgram(f, module, 0, elf, elfOffset);
+     };
+
     /** Load some programs into memory */
     private void LoadELFProgram(ByteBuffer f, SceModule module, int baseAddress,
         Elf32 elf, int elfOffset) throws IOException {
@@ -447,6 +458,12 @@ public class Loader {
 
         Memory.log.debug(String.format("PH alloc consumption %08X (mem %08X)", (module.loadAddressHigh - module.loadAddressLow), module.bss_size));
     }
+
+    private void LoadELFSections_630(ByteBuffer f, SceModule module, int baseAddress,
+        Elf32 elf, int elfOffset) throws IOException {
+        Emulator.log.info("6.30 firmware version's ELF sections detected!");
+         LoadELFSections(f, module, 0, elf, elfOffset);
+     };
 
     /** Load some sections into memory */
     private void LoadELFSections(ByteBuffer f, SceModule module, int baseAddress,

src/jpcsp/MainGUI.form
@@ -2,6 +2,10 @@
 
 <Form version="1.3" maxVersion="1.6" type="org.netbeans.modules.form.forminfo.JFrameFormInfo">
   <NonVisualComponents>
+    <Component class="javax.swing.ButtonGroup" name="filtersGroup">
+    </Component>
+    <Component class="javax.swing.ButtonGroup" name="resGroup">
+    </Component>
     <Menu class="javax.swing.JMenuBar" name="MenuBar">
       <SubComponents>
         <Menu class="javax.swing.JMenu" name="FileMenu">
@@ -180,6 +184,154 @@
                       </Properties>
                       <Events>
                         <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="ThreeItemActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JMenuItem" name="FullItem">
+                      <Properties>
+                        <Property name="accelerator" type="javax.swing.KeyStroke" editor="org.netbeans.modules.form.editors.KeyStrokeEditor">
+                          <KeyStroke key="F11"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="Full Screen"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="FullItemActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                  </SubComponents>
+                </Menu>
+                <Menu class="javax.swing.JMenu" name="FiltersMenu">
+                  <Properties>
+                    <Property name="text" type="java.lang.String" value="Filters"/>
+                  </Properties>
+                  <SubComponents>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="noneCheck">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="filtersGroup"/>
+                        </Property>
+                        <Property name="selected" type="boolean" value="true"/>
+                        <Property name="text" type="java.lang.String" value="None"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="noneCheckActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="bilinearCheck">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="filtersGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="Bilinear"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="bilinearCheckActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                  </SubComponents>
+                </Menu>
+                <Menu class="javax.swing.JMenu" name="ResolutionMenu">
+                  <Properties>
+                    <Property name="text" type="java.lang.String" value="Resolution"/>
+                  </Properties>
+                  <SubComponents>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck1">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="selected" type="boolean" value="true"/>
+                        <Property name="text" type="java.lang.String" value="480x272"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck1ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck2">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="800x600"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck2ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck3">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="960x544"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck3ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck4">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="1240x768"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck4ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck5">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="1152x768"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck5ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck6">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="1280x720"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck6ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck7">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="1280x768"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck7ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck8">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="1366x768"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck8ActionPerformed"/>
+                      </Events>
+                    </MenuItem>
+                    <MenuItem class="javax.swing.JCheckBoxMenuItem" name="resCheck9">
+                      <Properties>
+                        <Property name="buttonGroup" type="javax.swing.ButtonGroup" editor="org.netbeans.modules.form.RADComponent$ButtonGroupPropertyEditor">
+                          <ComponentRef name="resGroup"/>
+                        </Property>
+                        <Property name="text" type="java.lang.String" value="1440x816"/>
+                      </Properties>
+                      <Events>
+                        <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="resCheck9ActionPerformed"/>
                       </Events>
                     </MenuItem>
                   </SubComponents>

commit 44897679f83ed65cceb549626b1e4ce5608ae103

Re-enabled JpcspConnector to decrypt large PGD files (was broken since r2006). Fixed sceIoChangeAsyncPriority: reschedule correctly the async thread.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@2013 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/IoFileMgrForUser.java
@@ -167,7 +167,6 @@ public class IoFileMgrForUser implements HLEModule, HLEStartModule {
     private int defaultAsyncPriority;
     private final static int asyncThreadRegisterArgument = 16; // $s0 is preserved across calls
 
-    private byte[] AES128Key = new byte[16];
     private PGDFileConnector pgdFileConnector;
 
     // Implement the list of IIoListener as an array to improve the performance
@@ -1640,22 +1639,22 @@ public class IoFileMgrForUser implements HLEModule, HLEStartModule {
                             pgdFileConnector = new PGDFileConnector();
                         }
 
+                        // Store the key.
+                        byte[] keyBuf = new byte[0x10];
+                        for (int i = 0; i < 0x10; i++) {
+                            keyBuf[i] = (byte) mem.read8(indata_addr + i);
+                            keyHex += String.format("%02x", keyBuf[i] & 0xFF);
+                        }
+
                         try {
                             SeekableRandomFile decFile = new SeekableRandomFile(PGDFileConnector.decryptedFileName, "rw");
                             int fileLength = (int) info.readOnlyFile.length();
                             byte[] inBuf = new byte[fileLength];
                             byte[] outBuf = new byte[fileLength];
                             byte[] headerBuf = new byte[0x30 + 0x10];
-                            byte[] keyBuf = new byte[0x10];
                             byte[] hashBuf = new byte[0x10];
 
                             info.readOnlyFile.readFully(inBuf);
-
-                            // Store the key.
-                            for (int i = 0; i < 0x10; i++) {
-                                keyBuf[i] = (byte) mem.read8(indata_addr + i);
-                                keyHex += String.format("%02x", keyBuf[i] & 0xFF);
-                            }
 
                             // Decrypt 0x30 bytes at offset 0x30 to expose the first header.
                             System.arraycopy(inBuf, 0x10, headerBuf, 0, 0x10);
@@ -1679,9 +1678,17 @@ public class IoFileMgrForUser implements HLEModule, HLEStartModule {
 
                             decFile.write(crypto.DecryptPGD(outBuf, dataSize + 0x10, keyBuf));
                             decFile.close();
+                        } catch (OutOfMemoryError e) {
+                            // Ignore.
                         } catch (Exception e) {
                             // Ignore.
                         }
+
+                        try {
+							info.readOnlyFile.seek(info.position);
+						} catch (IOException e) {
+							log.error(e);
+						}
 
                         if (log.isDebugEnabled()) {
                             log.debug("hleIoIoctl get AES key " + keyHex);
@@ -1818,9 +1825,8 @@ public class IoFileMgrForUser implements HLEModule, HLEStartModule {
                 if (log.isDebugEnabled()) {
                     log.debug(String.format("sceIoChangeAsyncPriority changing priority of async thread from fd=%x to %d", info.uid, priority));
                 }
-                info.asyncThread.currentPriority = priority;
                 cpu.gpr[2] = 0;
-                Modules.ThreadManForUserModule.hleRescheduleCurrentThread();
+                Modules.ThreadManForUserModule.hleKernelChangeThreadPriority(info.asyncThread, priority);
             } else {
                 log.warn("sceIoChangeAsyncPriority invalid fd=" + uid);
                 cpu.gpr[2] = SceKernelErrors.ERROR_KERNEL_BAD_FILE_DESCRIPTOR;

src/jpcsp/HLE/modules150/ThreadManForUser.java
@@ -1101,6 +1101,41 @@ public class ThreadManForUser implements HLEModule, HLEStartModule {
         }
     }
 
+    public void hleKernelChangeThreadPriority(SceKernelThreadInfo thread, int newPriority) {
+    	if (thread == null) {
+    		return;
+    	}
+
+    	int oldPriority = thread.currentPriority;
+        thread.currentPriority = newPriority;
+        // switch in the target thread if it's now higher priority
+        if ((thread.status & PSP_THREAD_READY) == PSP_THREAD_READY &&
+        		newPriority < currentThread.currentPriority) {
+            if (log.isDebugEnabled()) {
+                log.debug("hleKernelChangeThreadPriority yielding to thread with higher priority");
+            }
+            needThreadReschedule = true;
+            hleRescheduleCurrentThread();
+        } else if ((thread.status & PSP_THREAD_READY) == PSP_THREAD_READY &&
+            		newPriority == oldPriority) {
+            if (log.isDebugEnabled()) {
+                log.debug("hleKernelChangeThreadPriority yielding to thread with same priority");
+            }
+            // Move the thread to the end of the list
+        	removeFromReadyThreads(thread);
+            addToReadyThreads(thread, false);
+            needThreadReschedule = true;
+            hleRescheduleCurrentThread();
+        } else if (thread.uid == currentThread.uid && newPriority > oldPriority) {
+            if (log.isDebugEnabled()) {
+                log.debug("hleKernelChangeThreadPriority rescheduling");
+            }
+            // yield if we moved ourself to lower priority
+            needThreadReschedule = true;
+            hleRescheduleCurrentThread();
+        }
+    }
+
     /**
      * Change to state of a thread.
      * This function must be used when changing the state of a thread as
@@ -3865,29 +3900,8 @@ public class ThreadManForUser implements HLEModule, HLEStartModule {
             if (log.isDebugEnabled()) {
                 log.debug("sceKernelChangeThreadPriority SceUID=" + Integer.toHexString(uid) + " newPriority:0x" + Integer.toHexString(priority) + " oldPriority:0x" + Integer.toHexString(thread.currentPriority));
             }
-            int oldPriority = thread.currentPriority;
-            thread.currentPriority = priority;
             cpu.gpr[2] = 0;
-            // switch in the target thread if it's now higher priority
-            if ((thread.status & PSP_THREAD_READY) == PSP_THREAD_READY &&
-                    priority < currentThread.currentPriority) {
-                if (log.isDebugEnabled()) {
-                    log.debug("sceKernelChangeThreadPriority yielding to thread with higher priority");
-                }
-                hleRescheduleCurrentThread();
-            } else if (uid == currentThread.uid && priority > oldPriority) {
-                if (log.isDebugEnabled()) {
-                    log.debug("sceKernelChangeThreadPriority rescheduling");
-                }
-                // yield if we moved ourself to lower priority
-                hleRescheduleCurrentThread();
-            } else if (priority == oldPriority) {
-                if (log.isDebugEnabled()) {
-                    log.debug("sceKernelChangeThreadPriority yielding to thread with same priority");
-                }
-                // yield to a thread having the same priority
-                hleRescheduleCurrentThread();
-            }
+            hleKernelChangeThreadPriority(thread, priority);
         }
     }
 