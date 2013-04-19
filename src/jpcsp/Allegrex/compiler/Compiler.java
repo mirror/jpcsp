mirror/jpcsp

commit 042a7eb1a68fde41e5b186795f60783e9823a6a8

Improved the compilation of sequences of "LW" or "SW" instructions: small performance improvement.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3108 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/Allegrex/compiler/Compiler.java
@@ -67,8 +67,8 @@ import org.xml.sax.SAXException;
  *     - is loading $sp address to another register
  *     - is reading Rs
  *     - is reading Rt
- *     - is writing Rt
- *     - is writing Rd
+ *     - is writing Rt [DONE]
+ *     - is writing Rd [DONE]
  *
  * TODO Ideas to further enhance performance:
  * - store stack variables "nn(sp)" into local variables

src/jpcsp/Allegrex/Instructions.java
@@ -1,4 +1,6 @@
-/* This file is part of jpcsp.
+/*
+This file is part of jpcsp.
+
 Jpcsp is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -35,7 +37,10 @@ import jpcsp.Allegrex.compiler.CodeInstruction;
 import jpcsp.Allegrex.compiler.Compiler;
 import jpcsp.Allegrex.compiler.ICompilerContext;
 import jpcsp.Allegrex.compiler.RuntimeContext;
+import jpcsp.Allegrex.compiler.SequenceLWCodeInstruction;
+import jpcsp.Allegrex.compiler.SequenceSWCodeInstruction;
 import jpcsp.HLE.SyscallHandler;
+import jpcsp.util.Utilities;
 
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
@@ -3922,9 +3927,62 @@ public void interpret(Processor processor, int insn) {
 @Override
 public void compile(ICompilerContext context, int insn) {
 	if (!context.isRtRegister0()) {
-		context.prepareRtForStore();
-		context.memRead32(context.getRsRegisterIndex(), context.getImm16(true));
-		context.storeRt();
+		int rs = context.getRsRegisterIndex();
+		int simm16 = context.getImm16(true);
+
+		int countSequence = 1;
+	    int[] offsets = null;
+	    int[] registers = null;
+
+	    if (!context.getCodeInstruction().isDelaySlot() && context.getRtRegisterIndex() != rs) {
+	        int address = context.getCodeInstruction().getAddress() + 4;
+		    // Compare LW opcode and rs register
+		    final int opcodeMask = 0xFFE00000;
+		    for (int i = 1; true; i++, address += 4) {
+		    	CodeInstruction nextCodeInstruction = context.getCodeInstruction(address);
+		    	boolean isSequence = false;
+		    	if (nextCodeInstruction != null && !nextCodeInstruction.isBranchTarget()) {
+		    		if ((nextCodeInstruction.getOpcode() & opcodeMask) == (insn & opcodeMask)) {
+		    			if (nextCodeInstruction.getRtRegisterIndex() != rs) {
+			    			if (offsets == null) {
+			    				offsets = new int[2];
+			    				registers = new int[2];
+			    				offsets[0] = simm16;
+			    				registers[0] = context.getRtRegisterIndex();
+			    			} else {
+			    				offsets = Utilities.extendArray(offsets, 1);
+			    				registers = Utilities.extendArray(registers, 1);
+			    			}
+			    			offsets[i] = nextCodeInstruction.getImm16(true);
+			    			registers[i] = nextCodeInstruction.getRtRegisterIndex();
+			    			isSequence = true;
+		    			}
+		    		}
+		    	}
+
+		    	if (!isSequence) {
+		    		break;
+		    	}
+				countSequence++;
+		    }
+	    }
+
+		if (countSequence > 1 && context.compileLWsequence(rs, offsets, registers)) {
+			if (countSequence > 1) {
+		    	if (Compiler.log.isDebugEnabled()) {
+		            CodeInstruction sequence = new SequenceLWCodeInstruction(rs, offsets, registers);
+		            sequence.setAddress(context.getCodeInstruction().getAddress());
+		    		Compiler.log.debug(String.format("CodeInstruction.compile %s", sequence));
+		    	}
+
+		    	// Skip the next lw instructions
+		    	context.skipInstructions(countSequence - 1, false);
+			}
+		} else {
+			context.prepareRtForStore();
+			context.memRead32(rs, simm16);
+			context.storeRt();
+		}
 	}
 }
 @Override
@@ -4094,9 +4152,58 @@ public void interpret(Processor processor, int insn) {
 public void compile(ICompilerContext context, int insn) {
 	int rs = context.getRsRegisterIndex();
 	int simm16 = context.getImm16(true);
-	context.prepareMemWrite32(rs, simm16);
-	context.loadRt();
-	context.memWrite32(rs, simm16);
+
+    int countSequence = 1;
+    int[] offsets = null;
+    int[] registers = null;
+
+    if (!context.getCodeInstruction().isDelaySlot()) {
+        int address = context.getCodeInstruction().getAddress() + 4;
+	    // Compare SW opcode and rs register
+	    final int opcodeMask = 0xFFE00000;
+	    for (int i = 1; true; i++, address += 4) {
+	    	CodeInstruction nextCodeInstruction = context.getCodeInstruction(address);
+	    	boolean isSequence = false;
+	    	if (nextCodeInstruction != null && !nextCodeInstruction.isBranchTarget()) {
+	    		if ((nextCodeInstruction.getOpcode() & opcodeMask) == (insn & opcodeMask)) {
+	    			if (offsets == null) {
+	    				offsets = new int[2];
+	    				registers = new int[2];
+	    				offsets[0] = simm16;
+	    				registers[0] = context.getRtRegisterIndex();
+	    			} else {
+	    				offsets = Utilities.extendArray(offsets, 1);
+	    				registers = Utilities.extendArray(registers, 1);
+	    			}
+	    			offsets[i] = nextCodeInstruction.getImm16(true);
+	    			registers[i] = nextCodeInstruction.getRtRegisterIndex();
+	    			isSequence = true;
+	    		}
+	    	}
+
+	    	if (!isSequence) {
+	    		break;
+	    	}
+			countSequence++;
+	    }
+    }
+
+	if (countSequence > 1 && context.compileSWsequence(rs, offsets, registers)) {
+		if (countSequence > 1) {
+	    	if (Compiler.log.isDebugEnabled()) {
+	            CodeInstruction sequence = new SequenceSWCodeInstruction(rs, offsets, registers);
+	            sequence.setAddress(context.getCodeInstruction().getAddress());
+	    		Compiler.log.debug(String.format("CodeInstruction.compile %s", sequence));
+	    	}
+
+	    	// Skip the next sw instructions
+	    	context.skipInstructions(countSequence - 1, false);
+		}
+	} else {
+		context.prepareMemWrite32(rs, simm16);
+		context.loadRt();
+		context.memWrite32(rs, simm16);
+	}
 }
 @Override
 public String disasm(int address, int insn) {

src/jpcsp/Allegrex/compiler/CodeInstruction.java
@@ -40,6 +40,7 @@ public class CodeInstruction {
 	private int branchingTo;
 	private boolean isBranching;
 	private Label label;
+	private boolean isDelaySlot;
 
     protected CodeInstruction() {
     }
@@ -119,7 +120,15 @@ public class CodeInstruction {
 		this.branchingTo = branchingTo;
 	}
 
-    public Label getLabel(boolean isBranchTarget) {
+	public boolean isDelaySlot() {
+		return isDelaySlot;
+	}
+
+	public void setIsDelaySlot(boolean isDelaySlot) {
+		this.isDelaySlot = isDelaySlot;
+	}
+
+	public Label getLabel(boolean isBranchTarget) {
         if (label == null) {
             label = new Label();
             if (isBranchTarget) {
@@ -230,6 +239,7 @@ public class CodeInstruction {
         	Compiler.log.warn(String.format("Instruction in a delay slot having a delay slot: %s", toString()));
         }
 
+        delaySlotCodeInstruction.setIsDelaySlot(true);
         Label delaySlotLabel = null;
         if (delaySlotCodeInstruction.hasLabel()) {
         	delaySlotLabel = delaySlotCodeInstruction.getLabel();

src/jpcsp/Allegrex/compiler/CompilerContext.java
@@ -37,6 +37,7 @@ import java.util.TreeSet;
 import jpcsp.Emulator;
 import jpcsp.Memory;
 import jpcsp.Processor;
+import jpcsp.State;
 import jpcsp.Allegrex.Common;
 import jpcsp.Allegrex.CpuState;
 import jpcsp.Allegrex.GprState;
@@ -73,6 +74,7 @@ import jpcsp.HLE.modules.HLEModuleFunction;
 import jpcsp.HLE.modules.HLEModuleManager;
 import jpcsp.HLE.modules.ThreadManForUser;
 import jpcsp.hardware.Interrupts;
+import jpcsp.memory.FastMemory;
 import jpcsp.memory.SafeFastMemory;
 import jpcsp.util.ClassAnalyzer;
 import jpcsp.util.DurationStatistics;
@@ -3503,6 +3505,10 @@ public class CompilerContext implements ICompilerContext {
 		if (Profiler.isProfilerEnabled()) {
 			return;
 		}
+		// Disable optimizations when the debugger is open
+		if (State.debugger != null) {
+			return;
+		}
 
 		int decreaseSpInstruction = -1;
 		int stackSize = 0;
@@ -3639,6 +3645,60 @@ public class CompilerContext implements ICompilerContext {
 		}
 	}
 
+	/**
+	 * Compile a sequence
+	 *     sw  $zr, n($reg)
+	 *     sw  $zr, n+4($reg)
+	 *     sw  $zr, n+8($reg)
+	 *     ...
+	 * into
+	 *     System.arraycopy(FastMemory.zero, 0, memoryInt, (n + $reg) >> 2, length)
+	 * 
+	 * @param baseRegister
+	 * @param offsets
+	 * @param registers
+	 * @return true  if the sequence could be compiled
+	 *         false if the sequence could not be compiled
+	 */
+	private boolean compileSWsequenceZR(int baseRegister, int[] offsets, int[] registers) {
+		for (int i = 0; i < registers.length; i++) {
+			if (registers[i] != _zr) {
+				return false;
+			}
+		}
+
+		for (int i = 1; i < offsets.length; i++) {
+			if (offsets[i] != offsets[i - 1] + 4) {
+				return false;
+			}
+		}
+
+		int offset = offsets[0];
+		int length = offsets.length;
+		do {
+	    	int copyLength = Math.min(length, FastMemory.zero.length);
+			// Build parameters for
+	    	//    System.arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+	    	// i.e.
+	    	//    System.arraycopy(FastMemory.zero,
+			//                     0,
+			//                     RuntimeContext.memoryInt,
+	    	//                     RuntimeContext.checkMemoryRead32(baseRegister + offset, pc) >>> 2,
+	    	//                     copyLength);
+    		mv.visitFieldInsn(Opcodes.GETSTATIC, Type.getInternalName(FastMemory.class), "zero", "[I");
+	    	loadImm(0);
+	    	loadMemoryInt();
+	    	prepareMemIndex(baseRegister, offset, false, 32);
+	    	loadImm(copyLength);
+	    	mv.visitMethodInsn(Opcodes.INVOKESTATIC, Type.getInternalName(System.class), "arraycopy", arraycopyDescriptor);
+
+	    	length -= copyLength;
+	    	offset += copyLength;
+		} while (length > 0);
+
+		return true;
+	}
+
 	private boolean compileSWLWsequence(int baseRegister, int[] offsets, int[] registers, boolean isLW) {
 		// Optimization only possible for memoryInt
 		if (RuntimeContext.memoryInt == null) {
@@ -3647,6 +3707,16 @@ public class CompilerContext implements ICompilerContext {
 		// Disable optimizations when the profiler is enabled.
 		if (Profiler.isProfilerEnabled()) {
 			return false;
+		}
+		// Disable optimizations when the debugger is open
+		if (State.debugger != null) {
+			return false;
+		}
+
+		if (!isLW) {
+			if (compileSWsequenceZR(baseRegister, offsets, registers)) {
+				return true;
+			}
 		}
 
 		int offset = offsets[0];
@@ -3662,11 +3732,13 @@ public class CompilerContext implements ICompilerContext {
     		}
 
     		if (isLW) {
-    			prepareRegisterForStore(rt);
-        		loadMemoryInt();
-        		loadTmp1();
-        		mv.visitInsn(Opcodes.IALOAD);
-        		storeRegister(rt);
+    			if (rt != _zr) {
+	    			prepareRegisterForStore(rt);
+	        		loadMemoryInt();
+	        		loadTmp1();
+	        		mv.visitInsn(Opcodes.IALOAD);
+	        		storeRegister(rt);
+    			}
     		} else {
     			loadMemoryInt();
     			loadTmp1();

src/jpcsp/Allegrex/compiler/Profiler.java
@@ -104,7 +104,8 @@ public class Profiler {
         }
 
         int count = 0;
-        log.info(String.format("Compilation time %dms, %d calls, average %.1fms, longest %dms", compilationTimeMicros / 1000, compilationCount, compilationTimeMicros / (double) compilationCount / 1000, longestCompilationTimeMicros / 1000));
+        double avg = compilationCount == 0 ? 0.0 : compilationTimeMicros / (double) compilationCount / 1000;
+        log.info(String.format("Compilation time %dms, %d calls, average %.1fms, longest %dms", compilationTimeMicros / 1000, compilationCount, avg, longestCompilationTimeMicros / 1000));
         log.info(String.format("CodeBlocks profiling information (%,d total cycles):", allCycles));
         for (CodeBlock codeBlock : sortedCodeBlocks) {
             long callCount = getCallCount(codeBlock);

commit 36f4292edaa6ed801a48e0b0a987c13c0522dc7f

Fixed some problems introduced by r3108: instructions were not marked as a branch target when they were only reached by a "J" instruction.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3110 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/Allegrex/compiler/Compiler.java
@@ -23,6 +23,8 @@ import static jpcsp.Allegrex.Common.Instruction.FLAG_STARTS_NEW_BLOCK;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -371,6 +373,7 @@ public class Compiler implements ICompiler {
         Stack<Integer> pendingBlockAddresses = new Stack<Integer>();
         pendingBlockAddresses.clear();
         pendingBlockAddresses.push(startAddress);
+        Set<Integer> branchingToAddresses = new HashSet<Integer>();
         while (!pendingBlockAddresses.isEmpty()) {
             int pc = pendingBlockAddresses.pop();
             if (!Memory.isAddressGood(pc)) {
@@ -432,6 +435,8 @@ public class Compiler implements ICompiler {
 	                        	// reached code has not been changed (i.e. invalidated).
                         		if (!memorySections.canWrite(branchingTo, false)) {
 	                        		pendingBlockAddresses.push(branchingTo);
+	                        	} else {
+	                        		branchingToAddresses.add(branchingTo);
 	                        	}
                         	} else {
                         		pendingBlockAddresses.push(branchingTo);
@@ -444,6 +449,10 @@ public class Compiler implements ICompiler {
 
                     isBranchTarget = false;
                 }
+            }
+
+            for (int branchingTo : branchingToAddresses) {
+            	codeBlock.setIsBranchTarget(branchingTo);
             }
         }
 