mirror/jpcsp

commit 6e34cc8f9322c8b3bc711db7064c8143f78be42d

Improve video command TRXKICK when performing a transfer to the GE. Added a WARNing when using a texture from the swizzled VRAM address space. Not sure if some application is using this address space.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3371 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/sceDisplay.java
@@ -1156,7 +1156,7 @@ public class sceDisplay extends HLEModule {
         boolean loadGEToScreen = !isUsingSoftwareRenderer() && !VideoEngine.getInstance().isSkipThisFrame();
 
         if (copyGEToMemory && (ge.getTopAddr() != topaddr || ge.getPixelFormat() != pixelformat)) {
-            copyGeToMemory(false);
+            copyGeToMemory(false, false);
             loadGEToScreen = true;
         }
 
@@ -1503,7 +1503,7 @@ public class sceDisplay extends HLEModule {
         }
     }
 
-    public void copyGeToMemory(boolean preserveScreen) {
+    public void copyGeToMemory(boolean preserveScreen, boolean forceCopyToMemory) {
         if (isUsingSoftwareRenderer()) {
             // GE is already in memory when using the software renderer
             return;
@@ -1521,6 +1521,10 @@ public class sceDisplay extends HLEModule {
             GETexture geTexture = GETextureManager.getInstance().getGETexture(re, ge.getTopAddr(), ge.getBufferWidth(), ge.getWidth(), ge.getHeight(), ge.getPixelFormat(), true);
             geTexture.copyScreenToTexture(re);
         } else {
+        	forceCopyToMemory = true;
+        }
+
+        if (forceCopyToMemory) {
             // Set texFb as the current texture
             re.bindTexture(resizedTexFb);
             re.setTextureFormat(ge.getPixelFormat(), false);

src/jpcsp/graphics/VideoEngine.java
@@ -2177,6 +2177,17 @@ public class VideoEngine {
             return;
         }
 
+        if (context.textureFlag.isEnabled() && !context.clearMode) {
+        	int textureAddr = context.texture_base_pointer[0] & Memory.addressMask;
+        	if (textureAddr > MemoryMap.END_VRAM && textureAddr < MemoryMap.START_VRAM + 0x800000) {
+        		if (isLogWarnEnabled) {
+        			log.warn(String.format("Texture in swizzled VRAM not supported 0x%08X", textureAddr));
+        		}
+        		endRendering(numberOfVertex);
+        		return;
+        	}
+        }
+
         if (type == GeCommands.PRIM_CONTINUE_PREVIOUS_PRIM) {
             // The PSP is continuing the previous PRIM command.
             // If the previous PRIM was a strip or a fan, the strip/fan is
@@ -2199,6 +2210,7 @@ public class VideoEngine {
             if (isLogDebugEnabled) {
                 log.debug(String.format("%s type %d unsufficient number of vertex %d", helper.getCommandString(PRIM), type, numberOfVertex));
             }
+            endRendering(numberOfVertex);
             return;
         }
 
@@ -3074,12 +3086,18 @@ public class VideoEngine {
 
         memoryForGEUpdated();
 
-        if (display.isGeAddress(context.textureTx_sourceAddress)) {
-            re.waitForRenderingCompletion();
-            display.copyGeToMemory(true);
+        boolean transferUsingMemcpy = false;
+        if (!display.isGeAddress(context.textureTx_destinationAddress) || bpp != bppGe || display.isUsingSoftwareRenderer()) {
+        	transferUsingMemcpy = true;
         }
 
-        if (!display.isGeAddress(context.textureTx_destinationAddress) || bpp != bppGe || display.isUsingSoftwareRenderer()) {
+        if (display.isGeAddress(context.textureTx_sourceAddress)) {
+            re.waitForRenderingCompletion();
+            // Force a copy to the memory if performing the transfer using memcpy
+            display.copyGeToMemory(true, transferUsingMemcpy);
+        }
+
+        if (transferUsingMemcpy) {
             if (isLogDebugEnabled) {
                 if (bpp != bppGe) {
                     log(helper.getCommandString(TRXKICK) + " BPP not compatible with GE");
@@ -5703,7 +5721,7 @@ public class VideoEngine {
             // If the texture is the current GE
             // first save the GE to memory before loading the texture.
             if (tex_addr == (context.fbp | MemoryMap.START_VRAM) && context.texture_storage == context.psm && context.texture_buffer_width[0] == context.fbw) {
-                display.copyGeToMemory(true);
+                display.copyGeToMemory(true, false);
                 // Re-bind the texture to be loaded, as the bind might have been changed during the GE copy.
                 re.bindTexture(context.currentTextureId);
             }

commit 494e81954b8ae3a13041deaccf837ed6c6248799

Fixed incorrect GE scaling introduced by r3371.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3447 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/sceDisplay.java
@@ -1615,7 +1615,7 @@ public class sceDisplay extends HLEModule {
             if (preserveScreen) {
                 // Redraw the screen
                 re.bindTexture(resizedTexFb);
-                drawFrameBuffer(fb, true, false, ge.getBufferWidth(), ge.getPixelFormat(), ge.getWidth(), ge.getHeight());
+                drawFrameBuffer(fb, false, false, ge.getBufferWidth(), ge.getPixelFormat(), ge.getWidth(), ge.getHeight());
             }
         }
 