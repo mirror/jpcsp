mirror/jpcsp

commit b5cb2f8195735bf1c1e695d73d5aa20f11d5a0d5

Draft implementation for screenshot generation from sceUtilityScreenshot. The screenshots are saved under ms0/PSP/SCREENSHOT.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3475 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/sceUtility.java
@@ -53,6 +53,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -105,6 +106,7 @@ import jpcsp.crypto.CryptoEngine;
 import jpcsp.filesystems.SeekableDataInput;
 import jpcsp.format.PSF;
 import jpcsp.graphics.RE.IRenderingEngine;
+import jpcsp.graphics.capture.CaptureImage;
 import jpcsp.hardware.MemoryStick;
 import jpcsp.hardware.Screen;
 import jpcsp.memory.IMemoryWriter;
@@ -1789,7 +1791,6 @@ public class sceUtility extends HLEModule {
     }
 
     protected static class ScreenshotUtilityDialogState extends UtilityDialogState {
-
         protected SceUtilityScreenshotParams screenshotParams;
 
         public ScreenshotUtilityDialogState(String name) {
@@ -1798,8 +1799,28 @@ public class sceUtility extends HLEModule {
 
         @Override
         protected boolean executeUpdateVisible() {
-            if (status == PSP_UTILITY_DIALOG_STATUS_VISIBLE) {
+            if (status == PSP_UTILITY_DIALOG_STATUS_VISIBLE && screenshotParams.isContModeOn()) {
                 status = PSP_UTILITY_DIALOG_STATUS_SCREENSHOT_UNKNOWN;
+            }
+
+            if (status == PSP_UTILITY_DIALOG_STATUS_VISIBLE) {
+            	Buffer buffer = Memory.getInstance().getBuffer(screenshotParams.imgFrameBufAddr, screenshotParams.imgFrameBufWidth * screenshotParams.displayHeigth * IRenderingEngine.sizeOfTextureType[screenshotParams.imgPixelFormat]);
+            	String directoyName = String.format("ms0/PSP/SCREENSHOT/%s/", screenshotParams.screenshotID);
+            	new File(directoyName).mkdirs();
+            	String fileName = null;
+            	for (int fileIndex = 1; fileIndex <= 9999; fileIndex++) {
+            		fileName = String.format("%sPJDEX_%04d.jpeg", directoyName, fileIndex);
+            		if (!new File(fileName).exists()) {
+            			break;
+            		}
+            	}
+            	CaptureImage captureImage = new CaptureImage(screenshotParams.imgFrameBufAddr, 0, buffer, screenshotParams.displayWidth, screenshotParams.displayHeigth, screenshotParams.imgFrameBufWidth, screenshotParams.imgPixelFormat, false, 0, false, true, null);
+            	captureImage.setFileName(fileName);
+            	try {
+					captureImage.write();
+				} catch (IOException e) {
+					log.error("sceUtilityScreenshot", e);
+				}
             }
 
             // TODO to be implemented

commit 3735b01aacffe9c27d85bdc549ecc936ee7aa40c

sceUtilityScreensoft: fixed regression issue from r3475, do not create a screenshot with CONT mode. Added dump of params structure for analysis.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3477 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/kernel/types/SceUtilityScreenshotParams.java
@@ -260,8 +260,12 @@ public class SceUtilityScreenshotParams extends pspAbstractMemoryMappedStructure
 		}
     }
 
-    public boolean isContModeOn() {
+    public boolean isContModeAuto() {
         return (startupType & 0x7) == PSP_UTILITY_SCREENSHOT_TYPE_CONT_AUTO;
+    }
+
+    public boolean isContModeFinish() {
+        return (startupType & 0x7) == PSP_UTILITY_SCREENSHOT_TYPE_CONT_FINISH;
     }
 
     @Override

src/jpcsp/HLE/modules150/sceUtility.java
@@ -1799,11 +1799,15 @@ public class sceUtility extends HLEModule {
 
         @Override
         protected boolean executeUpdateVisible() {
-            if (status == PSP_UTILITY_DIALOG_STATUS_VISIBLE && screenshotParams.isContModeOn()) {
+        	if (log.isDebugEnabled()) {
+        		log.debug(String.format("SceUtilityScreenshotParams %s", Utilities.getMemoryDump(paramsAddr.getAddress(), params.sizeof())));
+        	}
+
+        	if (status == PSP_UTILITY_DIALOG_STATUS_VISIBLE && screenshotParams.isContModeAuto()) {
                 status = PSP_UTILITY_DIALOG_STATUS_SCREENSHOT_UNKNOWN;
             }
 
-            if (status == PSP_UTILITY_DIALOG_STATUS_VISIBLE) {
+            if (status == PSP_UTILITY_DIALOG_STATUS_VISIBLE && !screenshotParams.isContModeAuto() && !screenshotParams.isContModeFinish() && Memory.isAddressGood(screenshotParams.imgFrameBufAddr)) {
             	Buffer buffer = Memory.getInstance().getBuffer(screenshotParams.imgFrameBufAddr, screenshotParams.imgFrameBufWidth * screenshotParams.displayHeigth * IRenderingEngine.sizeOfTextureType[screenshotParams.imgPixelFormat]);
             	String directoyName = String.format("ms0/PSP/SCREENSHOT/%s/", screenshotParams.screenshotID);
             	new File(directoyName).mkdirs();
@@ -1846,7 +1850,7 @@ public class sceUtility extends HLEModule {
                 log.info(String.format("%sContStart %s", name, params.toString()));
             }
 
-            if (!screenshotParams.isContModeOn()) {
+            if (!screenshotParams.isContModeAuto()) {
                 return SceKernelErrors.ERROR_SCREENSHOT_CONT_MODE_NOT_INIT;
             }
 